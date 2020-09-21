08516b63ceff92f6bb538e9d7b3c5312c530b2ec
VinceSilvestro <70409052+VinceSilvestro@users.noreply.github.com>
2020-09-20 23:37:03 -0400

    Add files via upload

---
+++ Lab4/hello.py
@@ -0,0 +1,12 @@
+#Vince Silvestro, 9/20/20, code by "Automate the Boring Stuff with Python." 
+# This program says/ hello and asks for my name.
+
+print('Hello world!')
+print('What is your name?')    # ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')    # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
